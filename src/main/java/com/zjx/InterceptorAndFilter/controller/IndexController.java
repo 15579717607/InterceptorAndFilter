7d8a9f663aad93d070bc45f4b8419d4ea40488ad
zjx <1364985298@qq.com>
2023-06-18 22:49:44 +0800

    第三次提交:hot-fix提交

--- src/main/java/com/zjx/InterceptorAndFilter/controller/IndexController.java
+++ src/main/java/com/zjx/InterceptorAndFilter/controller/IndexController.java
@@ -1,8 +1,11 @@
 package com.zjx.InterceptorAndFilter.controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * @author ZJX
  * @date 2023/6/17
  */
+@Controller
 public class IndexController {
 }
